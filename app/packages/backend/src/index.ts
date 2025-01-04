413b4323e89ec83052ccacf82ee6265eaf214fec
Brett Curtis <brett@osinfra.io>
2025-01-04 13:46:11 -0500

    Add custom authentication and GitHub organization transformers to backend

--- app/packages/backend/src/index.ts
+++ app/packages/backend/src/index.ts
@@ -7,6 +7,68 @@
  */
 
 import { createBackend } from '@backstage/backend-defaults';
+import { createBackendModule } from '@backstage/backend-plugin-api';
+import { githubOrgEntityProviderTransformsExtensionPoint } from '@backstage/plugin-catalog-backend-module-github-org';
+import { myTeamTransformer, myUserTransformer } from './transformers';
+
+import {
+  authProvidersExtensionPoint,
+  createProxyAuthProviderFactory,
+} from '@backstage/plugin-auth-node';
+import { gcpIapAuthenticator } from '@backstage/plugin-auth-backend-module-gcp-iap-provider';
+
+const customAuth = createBackendModule({
+  // This ID must be exactly "auth" because that's the plugin it targets
+  pluginId: 'auth',
+  // This ID must be unique, but can be anything
+  moduleId: 'custom-auth-provider',
+  register(reg) {
+    reg.registerInit({
+      deps: { providers: authProvidersExtensionPoint },
+      async init({ providers }) {
+        providers.registerProvider({
+          // This ID must match the actual provider config, e.g. addressing
+          // auth.providers.github means that this must be "github".
+          providerId: 'gcpiap',
+          // Use createProxyAuthProviderFactory instead if it's one of the proxy
+          // based providers rather than an OAuth based one
+          factory: createProxyAuthProviderFactory({
+            authenticator: gcpIapAuthenticator,
+            async signInResolver(info, ctx) {
+              /*********************************************************************
+               * Custom resolver code goes here, see farther down in this article! *
+               * "info" is the sign in result from the upstream (github here), and *
+               * "ctx" contains useful utilities for token issuance etc.           *
+               *********************************************************************/
+              return ctx.signInWithCatalogUser({
+                filter: {
+                  kind: ['User'],
+                  'spec.profile.email': info.result.iapToken.email as string,
+                },
+              });
+            },
+          }),
+        });
+      },
+    });
+  },
+});
+
+const githubOrgModule = createBackendModule({
+  pluginId: 'catalog',
+  moduleId: 'github-org-extensions',
+  register(env) {
+    env.registerInit({
+      deps: {
+        githubOrg: githubOrgEntityProviderTransformsExtensionPoint,
+      },
+      async init({ githubOrg }) {
+        githubOrg.setTeamTransformer(myTeamTransformer);
+        githubOrg.setUserTransformer(myUserTransformer);
+      },
+    });
+  },
+});
 
 const backend = createBackend();
 
@@ -21,7 +83,8 @@ backend.add(import('@backstage/plugin-auth-backend'));
 // See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 // See https://backstage.io/docs/auth/guest/provider
-backend.add(import('@backstage/plugin-auth-backend-module-gcp-iap-provider'));
+// backend.add(import('@backstage/plugin-auth-backend-module-gcp-iap-provider'));
+backend.add(customAuth);
 
 // catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend'));
@@ -53,5 +116,8 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend'));
+
+// github
+backend.add(githubOrgModule);
 
 backend.start();
