ef235423d444b9d3ee3d5bca7feb01384f227a6d
Brett Curtis <brett@osinfra.io>
2025-01-05 12:36:21 -0500

    Refactor custom authentication provider setup for clarity and remove commented code

--- app/packages/backend/src/index.ts
+++ app/packages/backend/src/index.ts
@@ -10,28 +10,17 @@ import {
 } from '@backstage/plugin-auth-node';
 
 const customAuth = createBackendModule({
-  // This ID must be exactly "auth" because that's the plugin it targets
   pluginId: 'auth',
-  // This ID must be unique, but can be anything
   moduleId: 'custom-auth-provider',
   register(reg) {
     reg.registerInit({
       deps: { providers: authProvidersExtensionPoint },
       async init({ providers }) {
         providers.registerProvider({
-          // This ID must match the actual provider config, e.g. addressing
-          // auth.providers.github means that this must be "github".
           providerId: 'gcpiap',
-          // Use createProxyAuthProviderFactory instead if it's one of the proxy
-          // based providers rather than an OAuth based one
           factory: createProxyAuthProviderFactory({
             authenticator: gcpIapAuthenticator,
             async signInResolver(info, ctx) {
-              /*********************************************************************
-               * Custom resolver code goes here, see farther down in this article! *
-               * "info" is the sign in result from the upstream (github here), and *
-               * "ctx" contains useful utilities for token issuance etc.           *
-               *********************************************************************/
               return ctx.signInWithCatalogUser({
                 filter: {
                   kind: ['User'],
